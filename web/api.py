332fc95c0ae2570c736f52ebef5293789975c71f
Frank Breedijk <frank@breedijk.net>
2024-08-02 18:18:19 +0200

    Need to normalize email to NFC and lower case

--- web/api.py
+++ web/api.py
@@ -3,36 +3,38 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
 import hashlib
+import unicodedata
 
 app = Flask(__name__, static_folder='static', static_url_path="/")
 
 def check_no_more_leaks(username: str,password: str) -> bool :
-    # Create SHA256 hash of the concatenated username and password
-    sha256_hash = hashlib.sha256(f'{username}{password}'.encode()).hexdigest()
+	username = unicodedata.normalize('NFC',username.lower())
+	# Create SHA256 hash of the concatenated username and password
+	sha256_hash = hashlib.sha256(f'{username}{password}'.encode()).hexdigest()
 
-    conn = sqlite3.connect('../data/nomoreleaks.sqlite3')
-    c = conn.cursor()
-    c.execute('SELECT hash FROM hashes WHERE hash = ?', (sha256_hash,))
-    result = c.fetchone()
-    conn.close()
-    if result:
-        return True
-    return False
+	conn = sqlite3.connect('../data/nomoreleaks.sqlite3')
+	c = conn.cursor()
+	c.execute('SELECT hash FROM hashes WHERE hash = ?', (sha256_hash,))
+	result = c.fetchone()
+	conn.close()
+	if result:
+		return True
+	return False
 
 @app.route('/login', methods=['POST'])
 
 def login():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+	data = request.get_json()
+	username = data.get('username')
+	password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'error': 'Username and password are required'}), 400
+	if not username or not password:
+		return jsonify({'error': 'Username and password are required'}), 400
 
-    result = {}
-    result["NoMoreLeaks"] = { "found": check_no_more_leaks(username, password) }
+	result = {}
+	result["NoMoreLeaks"] = { "found": check_no_more_leaks(username, password) }
 
-    return jsonify(result), 200
+	return jsonify(result), 200
 
 #@app.route('/')
 #def index():
@@ -40,4 +42,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
